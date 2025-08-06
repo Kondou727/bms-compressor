a66efab3e7ab714fad54f7c0137d1b671fdc1cf8
Kondou727 <kondoushinichi123765rr@gmail.com>
2025-08-06 23:26:12 +0800

    removed some unused functions

--- src/bms_file_reading.py
+++ src/bms_file_reading.py
@@ -1,18 +1,5 @@
 from pathlib import Path
 import os
-# filepath is absolute path to a specific file
-'''def get_audio_files(filepath):
-    filepath = Path(filepath)
-    audio_files = set()
-    with open(filepath, "r", encoding="latin-1") as file: # WTF?
-        for line in file:
-            if "MAIN DATA FIELD" in line:
-                break
-            if line.startswith("#WAV"):
-                left, right = line.strip().split(maxsplit=1)
-                audio_files.add(Path.joinpath(filepath.parent, right))
-    return audio_files
-'''
 # subdir should be a path object, return list of absolute paths of audio files in the subdir
 def get_audio_files(subdir):
     audio_files = []
@@ -38,13 +25,6 @@ def get_all_relative_paths(paths):
         output.add(path.parent)
     return output
 
-def replace_file_extension(filepath, output_file_path, input_extension, output_extension):
-    with open(filepath, 'r') as file:
-        filedata = file.read()
-    filedata = filedata.replace(input_extension, output_extension)
-    filepath = Path(filepath)
-    with open(output_file_path, 'w') as file:
-        file.write(filedata)
 
 if __name__ == "__main__":
     all_bms = list_all_files_with_extension(r"G:\nythil bms pack\eng\A", "bms")
