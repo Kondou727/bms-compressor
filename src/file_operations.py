6c407085a49b7d46fcb84ad80e8a6fce805f33a2
Kondou727 <kondoushinichi123765rr@gmail.com>
2025-08-07 16:03:00 +0800

    video conversion complete

--- src/file_operations.py
+++ src/file_operations.py
@@ -2,6 +2,7 @@ import shutil
 import os
 import stat
 from audio_conversion import to_ogg
+from video_conversion import to_mp4
 # make sure subdir is absolute path
 def process_files_subdirectory(subdir, output_path, base_path, in_place=False):
     if (subdir/ "processed_w") in subdir.iterdir():
@@ -16,6 +17,9 @@ def process_files_subdirectory(subdir, output_path, base_path, in_place=False):
                     except PermissionError:
                         os.chmod(file, stat.S_IWRITE)
                         os.remove(file)
+                if file.suffix.lower() in ['.mpg', '.avi', '.mp4', '.wmv', '.m1v', '.mpeg', '.m2v']:
+                    # since the name might be changed, the file deletion will be handled in the to_mp4 function
+                    to_mp4(file, base_path, base_path) 
             else:
                 full_output_path = output_path / file.relative_to(base_path)
                 if not full_output_path.exists():
--- src/video_conversion.py
+++ src/video_conversion.py
@@ -1,15 +1,35 @@
 import ffmpeg
 import subprocess
+import os
+import stat
 from pathlib import Path
 def to_mp4(input_file, output_path, base_path):
     relative_path = input_file.relative_to(base_path)
     output_file = output_path / relative_path.with_suffix('.mp4')
     print(f"{input_file} -> {output_file}")
-    '''if output_file.exists():
-        print("File already exists. Skipping...")
-        return'''
+    if output_file.exists():
+        new_path = input_file.with_stem(input_file.stem + "_old")
+        os.rename(input_file, input_file.with_stem(input_file.stem + "_old"))
+        input_file = new_path
     output_file.parent.mkdir(parents=True, exist_ok=True)
     ffmpeg.input(str(input_file)).output(str(output_file), vcodec='libx264', preset="slow", crf=36, an=None).run()
+    try:
+        os.remove(input_file)
+    except PermissionError:
+        os.chmod(input_file, stat.S_IWRITE)
+        os.remove(input_file)
+
+def get_length(filename):
+    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", filename],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    return float(result.stdout)
+
+
+
+
 # 2 pass option for filesize
 '''   bitrate = int(100000000 / get_length(input_file))
     ( # first pass
@@ -25,13 +45,6 @@ def to_mp4(input_file, output_path, base_path):
         .output(str(output_file), vcodec='libx264', video_bitrate=bitrate, **{"pass" : 2}, an=None)
         .run(overwrite_output=True)
     )'''
-def get_length(filename):
-    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                             "format=duration", "-of",
-                             "default=noprint_wrappers=1:nokey=1", filename],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-    return float(result.stdout)
 
 
 if __name__ == "__main__":
